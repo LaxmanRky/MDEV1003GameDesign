ab2265c65d3f34c4b0536dfde76800c04b7df447
LaxmanRky <laxman.rokaya02@gmail.com>
2025-03-31 16:59:02 -0400

    Added Scrolling Background

--- Assets/Scripts/BackgroundScroller.cs
+++ Assets/Scripts/BackgroundScroller.cs
@@ -1,6 +1,23 @@
 using UnityEngine;
 
-public class BackgroundScroller
+public class BackgroundScroller : MonoBehaviour
 {
-    
+    public float scrollSpeed = 2f;
+    public float resetPosition = -20f;  // When to reset the position
+    public float startPosition = 20f;   // Where to reset to
+
+    void Update()
+    {
+        // Move the background
+        transform.Translate(Vector3.left * scrollSpeed * Time.deltaTime);
+
+        // If the background has moved too far left
+        if (transform.position.x <= resetPosition)
+        {
+            // Reset its position
+            Vector3 newPos = transform.position;
+            newPos.x = startPosition;
+            transform.position = newPos;
+        }
+    }
 }
